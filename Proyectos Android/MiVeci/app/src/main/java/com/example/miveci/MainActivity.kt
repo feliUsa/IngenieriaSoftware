e411574080dcc0eb574d91abf6926a77204f65f5
Juan Andres Serrano Mesa <juan.serrano03@usa.edu.co>
2023-09-11 20:56:47 -0500

    Avance Front y Back

--- Proyectos Android/MiVeci/app/src/main/java/com/example/miveci/MainActivity.kt
+++ Proyectos Android/MiVeci/app/src/main/java/com/example/miveci/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.example.miveci
 
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import android.widget.TextView
 import java.util.Calendar
 
@@ -10,6 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mensaje_hora()
+        movimientoBurbujas()
     }
 
     //Funcion para mostrar mensaje segun hora por pantalla
@@ -22,5 +29,67 @@ class MainActivity : AppCompatActivity() {
             else -> "Buenas noches Veci"
         }
         Mensaje_de_saludo.text = mensaje
+    }
+
+    //funcion para llamar a la actividad de tiendas
+    fun callTienditasActivity(view: View) {
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        tiendasUuU()
+    }
+    fun tiendasUuU(){
+        val intent = Intent(this, tiendasActivity::class.java)
+        startActivity(intent)
+    }
+
+    //funcion para llamar a la actividad de mapa
+    fun callMapActivity(view: View) {
+        overridePendingTransition(R.anim.slide_in_right, R.anim.slide_out_left)
+        mapUuU()
+    }
+    fun mapUuU(){
+        val intent = Intent(this, mapActivity::class.java)
+        startActivity(intent)
+    }
+    //Funcion para llamar a la actividad de configuracion
+    fun callConfigActivity(view: View) {
+        configUuU()
+    }
+    fun configUuU(){
+        val intent = Intent(this, configActivity::class.java)
+        startActivity(intent)
+    }
+    //Animacion Movimiento burbujas
+    private fun movimientoBurbujas(){
+        val bubble1 = findViewById<ImageView>(R.id.burbuja1)
+        val bubble2 = findViewById<ImageView>(R.id.burbuja2)
+        val bubble3 = findViewById<ImageView>(R.id.burbuja3)
+        val bubble4 = findViewById<ImageView>(R.id.burbuja4)
+
+        val bubble1Animator = ObjectAnimator.ofFloat(bubble1, "translationY", 0f, 100f)
+        bubble1Animator.duration = 1000
+        bubble1Animator.repeatCount = ValueAnimator.INFINITE
+        bubble1Animator.repeatMode = ValueAnimator.REVERSE
+        bubble1Animator.start()
+
+        val bubble2Animator = ObjectAnimator.ofFloat(bubble2, "translationY", 0f, -100f)
+        bubble2Animator.duration = 1000
+        bubble2Animator.repeatCount = ValueAnimator.INFINITE
+        bubble2Animator.repeatMode = ValueAnimator.REVERSE
+        bubble2Animator.start()
+
+        val bubble3Animator = ObjectAnimator.ofFloat(bubble3, "translationY", 0f, -100f)
+        bubble3Animator.duration = 1000
+        bubble3Animator.repeatCount = ValueAnimator.INFINITE
+        bubble3Animator.repeatMode = ValueAnimator.REVERSE
+        bubble3Animator.start()
+
+        val bubble4Animator = ObjectAnimator.ofFloat(bubble4, "translationY", 0f, 100f)
+        bubble4Animator.duration = 1000
+        bubble4Animator.repeatCount = ValueAnimator.INFINITE
+        bubble4Animator.repeatMode = ValueAnimator.REVERSE
+        bubble4Animator.start()
+
+
+
     }
 }

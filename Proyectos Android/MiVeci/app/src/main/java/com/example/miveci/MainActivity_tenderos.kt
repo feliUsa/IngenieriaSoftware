a26f3cd2dc2ad10623cad44d664b5721e8ab2053
Juan Andres Serrano Mesa <juan.serrano03@usa.edu.co>
2023-09-20 20:59:26 -0500

    se modifico nombre archivo y tambien se creo login users

---
+++ Proyectos Android/MiVeci/app/src/main/java/com/example/miveci/MainActivity_tenderos.kt
@@ -0,0 +1,11 @@
+package com.example.miveci
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity_tenderos : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_tenderos)
+    }
+}
